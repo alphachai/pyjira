82934e520351fb74ef40edcb58ea3e9e5be0852d
Charlie Mathews <charlie@charliemathews.com>
2017-09-19 11:03:05 -0500

    Simplify example.

--- example.py
+++ example.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
 
-import os
-import pwd
 from collections import defaultdict
 from getpass import getpass
 
 from client.requests import IssueSearch, Issue
 
-#username = '{0}@mintel.com'.format(pwd.getpwuid(os.getuid()).pw_name)
 username = input('Atlassian Username: ')
 password = getpass('Password for {}: '.format(username))
 
-#query = 'project = MOAT and component in ("Survey System", "Custom Databases") and status in ("Planned", "In Progress", "Code Review", "On Hold") and type not in ("Epic") ORDER BY updated DESC',
-query = 'createdDate > -1d and project = MOAT and status in ("Incoming", "Triaged") and assignee = it-dev-maintenance-operations'
+query = 'project = MOAT and component in ("Survey System", "Custom Databases") and status in ("Planned", "In Progress", "Code Review", "On Hold") and type not in ("Epic") ORDER BY updated DESC',
+#query = 'createdDate > -1d and project = MOAT and status in ("Incoming", "Triaged") and assignee = it-dev-maintenance-operations'
 
 request = IssueSearch(
     auth=(username, password),
@@ -24,14 +21,10 @@ request.execute()
 results = defaultdict(list)
 
 for issue in request.response:
-    component = "Unknown"
-    if len(issue.components) > 0:
-        component = issue.components[0]
-    results[component].append(issue)
+    results[issue.status].append(issue)
 
-for component, issues in results.items():
-    print(component)
+for status, issues in results.items():
+    print(status)
     for i in issues:
         print(i)
 
-import pdb; pdb.set_trace()
